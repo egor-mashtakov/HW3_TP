2a6c1b32777d5464b31fb9b5110d0a56b0e7da74
egormashtakov <egor190707e@gmail.com>
2026-06-14 18:44:44 +0300

    Made a data generator with Dockerfile and run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+DATA_DIR="$(pwd)/data"
+LOCAL_DATA_DIR="$(pwd)/local_data"
+
+case "$1" in
+
+  build_generator)
+    docker build -t generator ./generator
+    ;;
+
+  run_generator)
+    mkdir -p "$DATA_DIR"
+    docker run --rm \
+      -v "$DATA_DIR":/data \
+      generator
+    ;;
+
+  create_local_data)
+    mkdir -p "$LOCAL_DATA_DIR"
+    python3 generator/generate.py "$LOCAL_DATA_DIR"
+    ;;
+esac
